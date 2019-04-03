ca71c58aa51d453c273be3b67cb21ebb8e9f9355
webgzh907247189 <18255106634@163.com>
2019-04-03 08:56:43 +0800

    update graphql

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,26 @@
 import * as Koa from 'koa';
 import {Route} from './router'
+import { ApolloServer, gql } from 'apollo-server-koa';
 
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+ 
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
 const app = new Koa();
+
+// applyMiddleware将graphql服务连接到koa框架
+server.applyMiddleware({ app });
+
 const cusRouter = new Route(app,`${__dirname}/controller`);
 cusRouter.init(); //注册路由
 
